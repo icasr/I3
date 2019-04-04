c925deaa133f215ba05984add0ae14b623fe4247
Matt Carter <m@ttcarter.com>
2019-04-05 09:34:06 +1000

    REFACTOR: More comprehensive dedupe tests (still under development though)

---
+++ cli/test/dedupe.js
@@ -0,0 +1,48 @@
+var _ = require('lodash');
+var expect = require('chai').expect;
+var spawn = require('child_process').spawn;
+var reflib = require('reflib');
+
+var dedupePath = '/home/mc/Papers/Projects/Node/sra-dedupe'; // Needs to point to where sra-dedupe lives on disk
+var inputFile = 'test/data/dupes.json';
+var outputFile = '/tmp/output.json';
+
+describe('Test dedupe App via CLI', function() {
+	this.timeout(60 * 1000); // 60s
+
+	var inputRefs;
+	before('parse input references', done => {
+		reflib.parseFile(inputFile, (err, refs) => {
+			inputRefs = refs;
+			done();
+		});
+	});
+
+	it('should launch the process and mark references as duplicates', done => {
+		var ps = spawn('node', [
+			'../cli/app.js',
+			'-vvv',
+			`--action=${dedupePath}`,
+			`--input=${inputFile}`,
+			`--output=${outputFile}`,
+			'--setting=action=mark',
+			'--setting=markField=label',
+		], {stdio: 'inherit'})
+		ps.on('exit', code => {
+			expect(code).to.be.equal(0);
+
+			reflib.parseFile(outputFile, (err, outputRefs) => {
+				expect(outputRefs).to.have.length(inputRefs.length);
+
+				// Field we ignore when comparing
+				var mangledFields = ['label', 'recNumber'];
+
+				// Only the label field should have changed
+				expect(inputRefs.map(ref => _.omit(ref, mangledFields))).to.deep.equal(outputRefs.map(ref => _.omit(ref, mangledFields)));
+
+				done();
+			});
+		});
+	});
+
+});
